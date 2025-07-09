a1c1e76eef3f30f401ac9e21f8732e7379806f77
paulo <paulojrzti@gmail.com>
2025-07-09 17:13:40 -0300

    feature/adicionando função de ad tarefas na aplicação

--- estudos-ts-toDolist/src/App.tsx
+++ estudos-ts-toDolist/src/App.tsx
@@ -1,89 +1,50 @@
-import { useState } from "react";
-import "./App.css";
+  import { useState } from "react";
+  import "./App.css";
 
-interface jogadorFutebol {
-  id: string;
-  nome: string;
-  idade: number;
-  posicao: string;
-}
+  interface toDoitem {
+    id: string;
+    texto: string;
+    concluido: boolean;
+  }
 
-function App() {
-  const [jogadores, setJogadores] = useState<jogadorFutebol[]>([]);
-  // Lista de jogadores armazenada no estado.
-  // `setJogadores` serve para adicionar novos jogadores à lista.
+  function App() {
+    const [toDo, setToDo] = useState<toDoitem[]>([]);
+    const [newToDo, setNewToDo] = useState<string>("");
+    
 
-  const [nome, setNome] = useState<string>("");
-  // Armazena o nome do novo jogador sendo cadastrado.
-
-  const [idade, setIdade] = useState<number | "">("");
-  // Armazena a idade. Começa como string vazia, mas será convertida para número.
-
-  const [posicao, setPosicao] = useState<string>("");
-  // Armazena a posição do novo jogador.
-
-  const adicionarJogador = () => {
-    // Função chamada ao clicar no botão de adicionar jogador
-
-    if (nome !== "" && idade !== "" && posicao !== "") {
-      // Verifica se todos os campos estão preenchidos
-
-      const novoJogador: jogadorFutebol = {
-        id: crypto.randomUUID(), // Gera um ID único para cada jogador
-        nome: nome, // Define o nome informado
-        idade: Number(idade), // Converte a idade para número
-        posicao: posicao, // Define a posição informada
-      };
-
-      setJogadores([...jogadores, novoJogador]);
-      // Adiciona o novo jogador à lista, mantendo os anteriores
-
-      setNome(""); // Limpa o campo de nome
-      setIdade(""); // Limpa o campo de idade
-      setPosicao(""); // Limpa o campo de posição
+    const adicionarTarefa = () => {
+      if (newToDo !== "") {
+        const newId = crypto.randomUUID();
+        const newToDoItem : toDoitem = {
+          id: newId,
+          texto: newToDo,
+          concluido: false
+        }
+        setToDo([...toDo, newToDoItem])
+        setNewToDo("")  
+      }
     }
-  };
 
-  return (
-    <>
-      <div className="app">
-        <div className="container">
-          <h1>lista de jogadores de futebol</h1>
-          <div className="inputs-container">
-            <input
-              type="text"
-              value={nome}
-              placeholder="nome"
-              onChange={(e) => setNome(e.target.value)}
-            />
-            <input
-              type="number"
-              value={idade}
-              placeholder="idade"
-              onChange={(e) =>
-                setIdade(e.target.value === "" ? "" : Number(e.target.value))
+    return (
+      <>
+        <div className="app">
+          <div className="container">
+            <h1>Lista de Tarefas</h1>
+            <div className="input-container">
+              <input type="text" placeholder="insira sua tarefa" value={newToDo} onChange={(e) => (setNewToDo(e.target.value))} />
+              <button onClick={adicionarTarefa}>Adicionar tarefa</button>
+            </div>
+            <ul>
+              {
+                toDo.map((toDoItem) => (
+                  <li key={toDoItem.id}> {toDoItem.texto}</li>
+                ) )
               }
-            />
-            <input
-              type="text"
-              value={posicao}
-              placeholder="posição"
-              onChange={(e) => setPosicao(e.target.value)}
-            />
-            <button onClick={adicionarJogador}> Adicionar jogador </button>
+            </ul>
           </div>
-          <ul>
-            {jogadores.map((jogador) => (
-              <li key={jogador.id}>
-                <strong>{jogador.nome}</strong> - {jogador.idade} anos, posição:
-                  {jogador.posicao}
-              </li>
-            ))}
-          </ul>
         </div>
-      </div>
-    </>
-  );
-}
+      </>
+    );
+  }
 
-export default App;
+  export default App;
